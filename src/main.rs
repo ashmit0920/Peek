35e5abf8f667eae082a0a7e78fe96661e6c509ba
unknown <ashmitthawait2@gmail.com>
2024-07-22 02:29:15 +0530

    added --name flag

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use clap::{Parser};
 use sysinfo::{System, Networks, Disks};
+use serde::{Deserialize, Serialize};
+use std::fs;
+use std::path::Path;
 
 #[derive(Parser)]
 #[clap(version = "1.0.0", author = "Ashmit athawait.work@gmail.com", about = "Displays system information")]
 
 struct Cli {
+    #[clap(long, help = "Store your name")]
+    name: Option<String>,
     #[clap(short, long, help = "Show CPU usage")]
     cpu: bool,
     #[clap(short, long, help = "Show memory usage")]
@@ -15,11 +20,38 @@ struct Cli {
     network: bool,
 }
 
+#[derive(Serialize, Deserialize)]
+struct UserData {
+    name: String,
+}
+
 fn main() {
     let args = Cli::parse();
 
     let mut system = System::new_all();
     system.refresh_all();
+
+    if let Some(name) = args.name {
+        let user_data = UserData { name };
+        let file_path = "user_data.json";
+
+        // Save the name to a JSON file
+        fs::write(file_path, serde_json::to_string(&user_data).unwrap()).expect("Unable to write file");
+
+        println!("\nNice to meet you {}!", user_data.name);
+    }
+
+    // Display stored name if it exists
+    let file_path = "user_data.json";
+    if Path::new(file_path).exists() {
+        let data: UserData = serde_json::from_str(&fs::read_to_string(file_path).expect("Unable to read file")).expect("Unable to parse JSON");
+        println!("\n---------- Welcome to Peekaboo! ----------");
+        println!("\nMonitoring {}'s System...", data.name);
+    }
+    else {
+        println!("\n---------- Welcome to Peekaboo! ----------");
+        println!("\nTip - Peekaboo can remember your name for personalised outputs! Just run 'peek --name YOUR_NAME'.");
+    }
 
     if args.cpu {
         println!("\nCPU Usage:");
